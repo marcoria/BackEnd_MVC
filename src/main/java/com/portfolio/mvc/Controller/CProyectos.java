633d92e6d46ebb194153e3e0ebea564da6e93df3
marcela vanesa coria <marcelavanesacoria@gmail.com>
2023-04-21 17:12:52 -0300

    cambio de url del front end

--- src/main/java/com/portfolio/mvc/Controller/CProyectos.java
+++ src/main/java/com/portfolio/mvc/Controller/CProyectos.java
@@ -26,7 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/proy")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://frontend2023.web.app")
 public class CProyectos {
     @Autowired
     SProyectos sProyectos;
--- src/main/java/com/portfolio/mvc/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/mvc/Security/Controller/AuthController.java
@@ -35,7 +35,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = "http://localhost:4200")//prueba con link de firebase
+@CrossOrigin(origins = "https://frontend2023.web.app")//prueba con link de firebase
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
